fed9d719977c82f485818a7e25d5c5f9e44ca607
Daiki Horiyama <d19921207@gmail.com>
2020-03-20 01:41:52 +0900

    tweak

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 # ローカル開発で使うユーザー
 # このユーザーに権限を付与する
-resource "aws_iam_user" "localuser" {
+resource "aws_iam_user" "example" {
   name = "localuser"
   path = "/personal/"
 
@@ -16,7 +16,7 @@ module "role_s3_putobject_for_ec2" {
   source = "./iam_role"
   name = "role_s3_putobject_for_ec2"
   service_identifier = "ec2.amazonaws.com"
-  iam_identifier = aws_iam_user.localuser.arn
+  iam_identifier = aws_iam_user.example.arn
   policy = data.aws_iam_policy_document.example.json
 }
 
@@ -30,7 +30,6 @@ data "aws_iam_policy_document" "example" {
     resources = [
       "${aws_s3_bucket.example.arn}/home/&{aws:username}",
     ]
-
   }
 }
 
